2958611fe5b27e8a32d4b0005b3719f256f3c1f9
poltm13 <poltm13@gmail.com>
2021-11-11 01:35:30 +0100

    webScraping integration script

---
+++ smartrend.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# File that integrates all scripts
+
+dir=./Logs/$(date '+%d-%m-%Y_%Hh-%Mm')
+mkdir -p $dir
+
+# Run media scraping
+node ./webScraping/media &> $dir/media-scraping.txt || { echo "Error: Media scraping execution failed. Logs at directory $dir"; exit 0; } 
+
+# TODO: Run image recognition
+
+# TODO: Run data processing
+
+# Run retail scraping
+node ./webScraping/retail &> $dir/retail-scraping.txt || { echo "Error: Retail scraping execution failed. Logs at directory $dir"; exit 0; } 
+
+# TODO: Run media upload
