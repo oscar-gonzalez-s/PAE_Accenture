caebe10f21ea5513bd6b7537b87a25f89b07c5e7
poltm13 <poltm13@gmail.com>
2021-11-17 08:23:53 +0100

    bash fix

--- smartrend.sh
+++ smartrend.sh
@@ -5,14 +5,14 @@ dir=./logs/$(date '+%d-%m-%Y_%Hh-%Mm')
 mkdir -p $dir
 
 # Run media scraping
-node ./webScraping/media &> $dir/media-scraping.txt || { echo "Error: Media scraping execution failed. Logs at directory $dir"; exit 0; } 
+node ./webscraping/media.js &> $dir/media-scraping.txt || { echo "Error: Media scraping execution failed. Logs at directory $dir"; exit 0; } 
 
 # TODO: Run image recognition
 
 # TODO: Run data processing
 
 # Run retail scraping
-node ./webScraping/retail &> $dir/retail-scraping.txt || { echo "Error: Retail scraping execution failed. Logs at directory $dir"; exit 0; }
+node ./webscraping/retail.js &> $dir/retail-scraping.txt || { echo "Error: Retail scraping execution failed. Logs at directory $dir"; exit 0; }
 
 echo Execution completed successfully
 
